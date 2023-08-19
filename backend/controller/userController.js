4a9a2c69f5baf339c92efef30f331bde5a09400a
Unais k <unais5676@gmail.com>
2023-08-19 19:55:32 +0530

    custom error handling by using middleware

---
+++ backend/controller/userController.js
@@ -0,0 +1,16 @@
+import asyncHandler from "express-async-handler";
+
+
+
+// ========================desc - Auth user/set token========================
+//========================route - POST api/users/auth========================
+//========================access- public========================
+const authUser = asyncHandler( async(req,res)=>{
+   
+    res.status(200).json({message:"Auth user"})
+});
+
+export {
+    authUser,
+}
+
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
+
+// ==========================================Import required modules and configuration files==========================================
 import express from "express";
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config(); // Load environment variables
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"; // Custom error handling middleware
 
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000; // Define the port for the server to listen on
 
+import userRoutes from "./routes/userRoutes.js"; // Import user-related routes
+
+// Create an instance of the Express application
 const app = express();
 
-app.get('/',(req,res)=> res.send("Server is ready"))
+// Set up routes for handling user-related API requests
+app.use('/api/users/', userRoutes);
 
-app.listen(port,()=> console.log(`Server started on port ${port}`))
+// Set up a basic route for the root URL
+app.get('/', (req, res) => res.send("Server is ready"));
+
+// Handle cases when no route matches the request with the 'notFound' middleware
+app.use(notFound);
+
+// Handle errors and provide consistent responses with the 'errorHandler' middleware
+app.use(errorHandler);
+
+// Start the Express server on the specified port
+app.listen(port, () => console.log(`Server started on port ${port}`));
