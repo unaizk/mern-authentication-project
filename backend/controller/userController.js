bf6e31367a5322bbcba70af3c25142c7bda24086
Unais k <unais5676@gmail.com>
2023-08-20 15:32:53 +0530

    create userModel and bcrypted password and now create jwt token for authentication in utils folder

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,6 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js"
+import generateToken from "../utils/generateToken.js";
 
 
 
@@ -14,7 +16,33 @@ const authUser = asyncHandler( async(req,res)=>{
 //========================route - POST api/users========================
 //========================access- public========================
 const registerUser = asyncHandler( async(req,res)=>{
-    res.status(200).json({message:"Register user"})
+    const {name,email,password} = req.body
+
+    const userExist = await User.findOne({email});
+
+    if(userExist){
+        res.status(400);
+        throw new Error('User already exisit')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        generateToken(res,user._id)
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email
+        })
+    }else{
+        res.status(400);
+        throw new Error('invalid user data')
+    }
+    
 });
 
 // ========================desc - Logout user========================
--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,21 @@ const port = process.env.PORT || 5000; // Define the port for the server to list
 
 import userRoutes from "./routes/userRoutes.js"; // Import user-related routes
 
-connectDB();
+connectDB(); //calling mongoDB connected function that is imported from config folder
 
 // Create an instance of the Express application
 const app = express();
+
+// Middleware to parse incoming JSON data in the request body.
+// This allows us to access the JSON data in route handlers using req.body.
+app.use(express.json());
+
+
+// Middleware to parse incoming URL-encoded data in the request body.
+// The extended option set to true allows handling rich objects and arrays in the URL-encoded data.
+// This middleware makes the parsed data available in route handlers using req.body.
+app.use(express.urlencoded({extended:true}))
+
 
 // Set up routes for handling user-related API requests
 app.use('/api/users/', userRoutes);
