1517379812659c5620d6b9151b3e33fdd55bb96e
Unais k <unais5676@gmail.com>
2023-08-20 15:47:48 +0530

    made the authenticate verification  when user login by jwt and password comparing

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -8,7 +8,23 @@ import generateToken from "../utils/generateToken.js";
 //========================route - POST api/users/auth========================
 //========================access- public========================
 const authUser = asyncHandler( async(req,res)=>{
-    res.status(200).json({message:"Auth user"})
+    const {email,password}= req.body;
+
+    const user = await User.findOne({email});
+
+    if(user && (await user.matchPaswword(password))){
+        generateToken(res,user._id)
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email
+        })
+    }else{
+        res.status(400);
+        throw new Error('invalid email or password')
+    }
+
+
 });
 
 
