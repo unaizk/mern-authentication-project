8c523396ca6a5c45c61c130584ed35508511c1f4
Unais k <unais5676@gmail.com>
2023-08-21 19:51:32 +0530

    display profile of user

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import { Link , useNavigate } from 'react-router-dom';
+import React,{ useState , useEffect } from 'react';
+import { Form,Button } from 'react-bootstrap';
+import { useDispatch,useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import {toast} from "react-toastify"
+import Loader from '../components/Loader';
+
+import { setCredentials } from '../slices/authSlice';
+
 
 const ProfileScreen = () => {
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('');
+    const [confirmPassword,setConfirmPassword] = useState('');
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const {userInfo} = useSelector((state)=>state.auth)
+
+    useEffect(()=>{
+      setName(userInfo.name)
+      setEmail(userInfo.email)
+      
+    },[userInfo.setName,userInfo.setEmail])
+
+    const submitHandler = async(e)=>{
+        e.preventDefault();
+        if(password !== confirmPassword){
+            toast.error('Password do not match')
+        }else{
+           console.log('submit');
+        }
+    }
   return (
-    <div>ProfileScreen</div>
+    <FormContainer>
+        <h1>Update Profile</h1>
+
+        <Form onSubmit={submitHandler}>
+            <Form.Group className='my-2' controlId='name'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control  type='text' placeholder='Enter Name' value={name} onChange={(e)=>setName(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group className='my-2' controlId='email'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control  type='email' placeholder='Enter Email' value={email} onChange={(e)=>setEmail(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group className='my-2' controlId='password'>
+                <Form.Label>Enter Password</Form.Label>
+                <Form.Control  type='password' placeholder='Enter Password' value={password} onChange={(e)=>setPassword(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group className='my-2' controlId='confirmPassword'>
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control  type='password' placeholder='Confirm Password' value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+                 
+            <Button type='submit' variant='primary' className='mt-3'>
+                Update
+            </Button>
+
+        </Form>
+
+    </FormContainer>
   )
 }
 
